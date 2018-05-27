b3b2d8c97a38711359812eeb3ff406ad43442749
lassiter <pikapp336@me.com>
2018-05-27 13:29:04 -0400

    Program file

---
+++ send_text.py
@@ -0,0 +1,3 @@
+#   Description:
+#       This is a mini project written in python from Udacity.com's Intro to Programming Nano Degree course. 
+#    This program sends a text message to an sms enabled device.
